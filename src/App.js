632bfec8c49a9f3885fcb4c164c91d787ef5f68d
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-02-26 19:57:44 -0500

    added images

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import AlexaImage from "./images/alexa.png";
 import CortonaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
 
-console.log("AlexaImage");
-console.log("SiriImage");
-
 
 
 function App() {
@@ -15,9 +12,10 @@ function App() {
 		<div>
 			<div>Personal Digital Assistants</div>
 
-			<ProfileCard title="Alexa" handle="@alexa69" />
-			<ProfileCard title="Cortana" handle="@cortana69" />
-			<ProfileCard title="Siri" handle="@siri69" />
+			<ProfileCard title="Alexa" handle="@alexa69" image={AlexaImage} />
+			<ProfileCard title="Cortana" handle="@cortana69" image={CortonaImage} />
+			<ProfileCard title="Siri" handle="@siri69" image={SiriImage} />
+
 		</div>
 	);
 }
